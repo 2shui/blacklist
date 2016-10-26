1957223a177736200c1b0561a1ba246a3a0bab47
zgj <zgjlovelife@gmail.com>
2016-10-26 17:23:35 +0800

    2016-10-26 17:23:33

---
+++ src/main/resource/resources/js/baidutj.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?030f8d1340752fe4534e7fd060fc79fa";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
