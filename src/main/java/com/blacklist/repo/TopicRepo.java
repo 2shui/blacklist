c1269c005743cf27dbbb6c5124b5d353e3318d8f
zgj <zgjlovelife@gmail.com>
2016-11-02 16:43:28 +0800

    2016-11-2 16:43:26

--- src/main/java/com/blacklist/repo/TopicRepo.java
+++ src/main/java/com/blacklist/repo/TopicRepo.java
@@ -1,5 +1,7 @@
 package com.blacklist.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,4 +16,10 @@ public interface TopicRepo extends JpaRepository<Topic, Long> {
 	@Modifying
 	@Query(value = "update it_topic t set t.access_num = t.access_num+1 where t.id=:id", nativeQuery = true)
 	public int addAccessNum(@Param("id") Long id);
+	
+	public Topic findByIdAndStatus(Long id, Integer status);
+	
+	public List<Topic> findByStatus(Integer status);
+	
+	public List<Topic> findByCompanyLike(String company);
 }
